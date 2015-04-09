e3e3ddfdab32730afd7ccc32f467844996033ff6
João Paulo Cercal <jpcercal@gmail.com>
2015-04-09 09:57:58 -0300

    Implementation of classes to namespace Exception.

---
+++ src/Exception/ResourceManagerRefusedUpdateException.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Cekurte package.
+ *
+ * (c) João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cekurte\ComponentBundle\Exception;
+
+/**
+ * ResourceManagerRefusedUpdateException
+ *
+ * @author João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * @version 2.0
+ */
+class ResourceManagerRefusedUpdateException extends ResourceManagerRefusedException
+{
+}
