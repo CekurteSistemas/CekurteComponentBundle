2829e3c7a3d3f88336d320c5631f9b5ea1eeaf1e
João Paulo Cercal <jpcercal@gmail.com>
2015-04-09 00:35:53 -0300

    Unused method, property, variable or parameter.

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -27,7 +27,8 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cekurte_component');
+
+        $treeBuilder->root('cekurte_component');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
